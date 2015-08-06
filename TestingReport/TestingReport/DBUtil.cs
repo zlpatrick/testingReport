b34f168645b34aa0fc6e8046b0d558e3d920d204
zhangpa <zhangpa@SHACND10874TJ.ads.autodesk.com>
2015-08-06 17:08:48 +0800

    first commit

---
+++ TestingReport/TestingReport/DBUtil.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace TestingReport
+{
+    public class DBUtil
+    {
+        string type = "oledb";
+        public DBUtil(string type)
+        {
+            this.type = type;
+        }
+        public OleDbConnection createOleDbConnection()
+        {
+            OleDbConnection conn = new OleDbConnection();
+            return conn;
+        }
+
+        public SqlConnection createSqlConnection()
+        {
+            SqlConnection conn = new SqlConnection();
+            return conn;
+        }
+
+        public DataSet executeOleQuery(string sql)
+        {
+            OleDbConnection conn = createOleDbConnection();
+
+            try
+            {
+                conn.Open();
+                OleDbDataAdapter ada = new OleDbDataAdapter(sql,conn);
+                DataSet ds = new DataSet();
+                ada.Fill(ds);
+                conn.Close();
+                return ds;
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public void executeOleNonQuery(string sql)
+        {
+            OleDbConnection conn = createOleDbConnection();
+            try
+            {
+                conn.Open();
+                OleDbCommand command = new OleDbCommand(sql, conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public DataSet executeSqlQuery(string sql)
+        {
+            SqlConnection conn = createSqlConnection();
+            try
+            {
+                conn.Open();
+                SqlDataAdapter ads = new SqlDataAdapter(sql, conn);
+                DataSet ds = new DataSet();
+                ads.Fill(ds);
+                conn.Close();
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public void executeSqlNonQuery(string sql)
+        {
+            SqlConnection conn = createSqlConnection();
+            try
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(sql, conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ TestingReport/TestingReport/vote.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestingReport
+{
+    public partial class vote : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox box1 = new TextBox();
+            this.form1.Controls.Add(box1);
+        }
+    }
+}
