5fa45ed6a68451adf56a3fe77ce78f321b5cd96a
Patrick Zhang <patrick.zhang@autodesk.com>
2015-09-21 17:55:08 +0800

    adminpage

---
+++ TestingReport/TestingReport/adminlogin.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="adminlogin.aspx.cs" Inherits="TestingReport.adminlogin" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+   <p>管理员ID：<asp:TextBox ID="adminName" runat="server"></asp:TextBox></p> 
+   <p>管理员密码：<asp:TextBox ID="adminPass" runat="server"></asp:TextBox></p>
+   <p><asp:Button ID="loginButton" Text="登录" runat="server" /></p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ TestingReport/TestingReport/adminlogin.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.
+
+namespace TestingReport
+{
+    public partial class adminlogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginButton_Click(object sender, EventArgs e)
+        {
+            string adminId = this.adminName.Text.Trim();
+            string adminpass = this.adminPass.Text;
+
+            DBUtil db = new DBUtil();
+            DataSet ds = db.executeSqlQuery("select * from Admins where adminId='" + adminId + "'");
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                string adminpassDb = ds.Tables[0].Rows[0]["adminpass"].ToString();
+                if(adminpassDb.Equals(System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(adminpass, "MD5"))
+                {
+                    Session["adminid"] = adminId;
+                    Response.Redirect("admin.aspx");
+                }
+                else
+                {
+                    ClientScript.RegisterStartupScript(Page.GetType(), "", "<script>alert('密码错误!')</script>");
+                }
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(Page.GetType(), "", "<script>alert('无此用户!')</script>");
+            }
+        }
+    }
+}
