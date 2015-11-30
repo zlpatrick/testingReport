ac0949f38e1020f7339bc8204a9e240508b6d761
Patrick Zhang <Patrick.Zhang@autodesk.com>
2015-11-30 19:02:17 +0800

    c

---
+++ TestingReport/TestingReport/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestingReport
+{
+    public class Entity
+    {
+    }
+
+    public class HistoryMeasures
+    {
+        public List<string> timepoint;
+        public List<int> measurescores;
+    }
+}
